1e24ac087432a548358022367207e3be49977270
Shri_Ram <shriram173yt@gmail.com>
2025-01-28 21:37:21 +0530

    UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-
-const Button = ()=>{
-  return <button onClick={()=>{console.log('Clicked Me')}}>Click Me</button>
-}
 const App = () => {
+
+  const [count , setCount] = useState(0);
+  const increment = ()=>{setCount(count+1)}
+  const decrement = ()=>{setCount(count-1)}
+
   return (
-    <Button></Button>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
   )
 }
 
