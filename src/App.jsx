854961ef0c14d2c2ec597c17ab736cc4df8c000f
Shri_Ram <shriram173yt@gmail.com>
2025-01-29 13:29:42 +0530

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
-
-  const [card,setcard] = useState({"Movie-Name":"Equaliser","Rating":10})
-
-  function changeRate(){
-    const copyMovie = {...card,"Rating":"e2dfefefe"}
-    setcard(copyMovie)
+  const [movieDetails,setmovieDetails] = useState([
+    {Id:1,MovieName : "Luffy",Rating:10},
+    {Id:2,MovieName : "Robin",Rating:4}
+  ])
+  function handleClick(){
+    setmovieDetails(movieDetails.map((movie)=>movie.Id == '1' ? {...movie,MovieName:"Nami"}: movie))
   }
+
+
   return (
     <div>
-      {card['Movie-Name']}
-      {card.Rating}
-      <button onClick={changeRate}>Click</button>
+      {
+      movieDetails.map((movie)=>(
+        <div>
+          <h1>{movie.MovieName}</h1>
+          <h1>{movie.Rating}</h1>
+          </div>
+        )) 
+        }
+
+      <button onClick={handleClick}>Click Me</button>
     </div>
   )
 }
