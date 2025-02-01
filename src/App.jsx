d94ec2d98132b332c7a0440d1b97509dcc1a4981
Shri_Ram <shriram173yt@gmail.com>
2025-02-01 19:43:45 +0530

    React-Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
-import Notify from './components/Notify';
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
 
 const App = () => {
-  const [isCopied,setisCopied] = useState(false);
+  const [count,setcount] = useState(0);
+  const [value,setvalue] = useState(0);
+
+  useEffect(()=>{
+    console.log('Use Effect')
+ 
+  },[count])
+
   return (
     <div>
-    <Notify copy={isCopied}></Notify>
-    <button onClick={()=>setisCopied(true)}>Click</button>
+      <h1>{count}</h1>
+      <h1>{value}</h1>
+      <button onClick={()=>{setcount(count+1)}}>Submit</button>
+      <br></br>
+      <button onClick={()=>{setvalue(value-1)}}>Value Submit</button>
     </div>
   )
 }
